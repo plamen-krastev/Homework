f1ccc2a3ec8c73ffc492214c9b441b2dca170861
plamen-krastev <plamen.krastev@gmail.com>
2024-01-06 13:10:24 +0200

    Associative Arrays - Exercise

--- Associative Arrays/Exercise/04-party-time.js
+++ Associative Arrays/Exercise/04-party-time.js
@@ -1,80 +1,88 @@
 function solve(arr) {
 
-    let party = new Set();
+    let party = [];
 
-    let index = arr.indexOf('PARTY');
+    // populate party array:
+    while (arr[0] != 'PARTY') {
+        let curGuest = arr.shift();
+        party.push(curGuest);
+    }
+    // console.log(party);
+    arr.shift(); // remove 'PARTY'
 
-    for (let i = 0; i < index; i++) {
-        let curEl = arr[i];
-        party.add(curEl);
+    // remove guests from array party:
+    for (let guest of arr) {
+        let index = party.indexOf(guest);
+        if (index != -1) {
+            party.splice(index, 1);
+        }
+    }
+    // console.log(party);
+
+    let vips = [];
+    let regulars = [];
+
+    for (let guest of party) {
+        if (guest.charCodeAt(0) >= 48 && guest.charCodeAt(0) <= 57) {
+            vips.push(guest);
+        } else {
+            regulars.push(guest);
+        }
     }
 
-    for (let i = (index + 1); i < arr.length; i++) {
-        let curEl = arr[i];
-        party.delete(curEl);
+    // print
+
+    console.log(party.length);
+    if (vips.length > 0) {
+        vips.sort((a, b) => (a - b));
+        console.log(vips.join('\n'));
     }
-
-    let guests = Array
-        .from(party)
-        .sort(((a, b) => (a - b)));
-
-    console.log(party.size);
-    console.log(guests.join('\n'));
+    if (regulars.length > 0) {
+        regulars.sort;
+        console.log(regulars.join('\n'));
+    }
 }
 
+solve(['7IK9Yo0h',
+'9NoBUajQ',
+'Ce8vwPmE',
+'SVQXQCbc',
+'tSzE5t0p',
+'PARTY',
+'9NoBUajQ',
+'Ce8vwPmE',
+'SVQXQCbc'
+]
+);
+
 solve([
-    '1OK9Yo0h',
-'2KK9Yo0h',
-'3LK9Yo0h',
-'4SK9Yo0h',
-'5TK9Yo0h',
-'0UK9Yo0h',
-
-'6WK9Yo0h',
-'7BK9Yo0h',
-'8AK9Yo0h',
-'9mK9Yo0h',
-'bbt1OK9Y',
-'BaA2KK9r',
-    '7IK9Yo0h',
-    '9NoBUajQ',
-    'Ce8vwPmE',
-    'SVQXQCbc',
-    'tSzE5t0p',
+    'm8rfQBvl',
+    'fc1oZCE0',
+    'UgffRkOn',
+    '7ugX7bm0',
+    '9CQBGUeJ',
+    '2FQZT3uC',
+    'dziNz78I',
+    'mdSGyQCJ',
+    'LjcVpmDL',
+    'fPXNHpm1',
+    'HTTbwRmM',
+    'B5yTkMQi',
+    '8N0FThqG',
+    'xys2FYzn',
+    'MDzcM9ZK',
     'PARTY',
-    '9NoBUajQ',
-    'Ce8vwPmE',
-    'SVQXQCbc'
+    '2FQZT3uC',
+    'dziNz78I',
+    'mdSGyQCJ',
+    'LjcVpmDL',
+    'fPXNHpm1',
+    'HTTbwRmM',
+    'B5yTkMQi',
+    '8N0FThqG',
+    'm8rfQBvl',
+    'fc1oZCE0',
+    'UgffRkOn',
+    '7ugX7bm0',
+    '9CQBGUeJ'
 ]);
-
-// solve([
-//     'm8rfQBvl',
-//     'fc1oZCE0',
-//     'UgffRkOn',
-//     '7ugX7bm0',
-//     '9CQBGUeJ',
-//     '2FQZT3uC',
-//     'dziNz78I',
-//     'mdSGyQCJ',
-//     'LjcVpmDL',
-//     'fPXNHpm1',
-//     'HTTbwRmM',
-//     'B5yTkMQi',
-//     '8N0FThqG',
-//     'xys2FYzn',
-//     'MDzcM9ZK',
-//     'PARTY',
-//     '2FQZT3uC',
-//     'dziNz78I',
-//     'mdSGyQCJ',
-//     'LjcVpmDL',
-//     'fPXNHpm1',
-//     'HTTbwRmM',
-//     'B5yTkMQi',
-//     '8N0FThqG',
-//     'm8rfQBvl',
-//     'fc1oZCE0',
-//     'UgffRkOn',
-//     '7ugX7bm0',
-//     '9CQBGUeJ'
-// ]);
