85a76621ab47741740f967b5cfc7f5e2125dfbc2
plamen-krastev <plamen.krastev@gmail.com>
2024-01-10 21:21:17 +0200

    Basic Syntax, Conditional Statements and Loops - Lab

--- Fundamentals - 2024/Basic Syntax, Conditional Statements and Loops/LAB/demo.js
+++ Fundamentals - 2024/Basic Syntax, Conditional Statements and Loops/LAB/demo.js
@@ -1,7 +1,23 @@
-function demo() {
-  let variable = 'Hello';
+function demo(number) {
+    let variable = 'Hello';
 
-  console.log(typeof variable);
+    console.log(typeof variable);
+
+    let num = 5;
+
+    for (let i = 0; i < 10; i++) {
+      let value = i + 5;
+      if (value > 10) {
+        break;
+      }
+      console.log(value);
+    }
+    if (num === 5) {
+        let value = num;
+        console.log(value);
+      }
+
+      console.log(number);
 }
 
-demo();
+demo(155);
