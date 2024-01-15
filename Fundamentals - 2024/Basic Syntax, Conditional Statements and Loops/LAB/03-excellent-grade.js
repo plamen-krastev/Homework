48d42faa30040e0f1abc51023121ead0d607eb1f
plamen-krastev <plamen.krastev@gmail.com>
2024-01-15 21:09:49 +0200

    Basic Syntax, Conditional Statements and Loops - Lab

---
+++ Fundamentals - 2024/Basic Syntax, Conditional Statements and Loops/LAB/03-excellent-grade.js
@@ -0,0 +1,12 @@
+function solve(num) {
+
+    if (num >= 5.5) {
+        console.log('Excellent');
+    } else {
+        console.log('Not excellent');
+    }
+
+}
+
+solve(5.50);
+solve(4.35);
---
+++ Fundamentals - 2024/Basic Syntax, Conditional Statements and Loops/LAB/04-foreign-language.js
@@ -0,0 +1,26 @@
+function solve(language) {
+
+    switch (language) {
+        case "USA":
+            console.log('English');
+            break;
+        case "England":
+            console.log('English');
+            break;
+        case "Spain":
+            console.log("Spanish");
+            break;
+        case "Argentina":
+            console.log("Spanish");
+            break;
+        case "Mexico":
+            console.log("Spanish");
+            break;
+
+        default:
+            console.log('unknown');;
+    }
+}
+
+solve('USA');
+solve('Germany');
