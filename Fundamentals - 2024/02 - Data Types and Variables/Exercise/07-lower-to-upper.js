7313fce53506bbc24ab7501ef25ccfc85bf99737
plamen-krastev <plamen.krastev@gmail.com>
2024-01-20 12:53:10 +0200

    Data Types and Variables - Exercise

---
+++ Fundamentals - 2024/02 - Data Types and Variables/Exercise/07-lower-to-upper.js
@@ -0,0 +1,11 @@
+function solve(char) {
+  if (char === char.toUpperCase()) {
+    console.log('upper-case');
+  } else {
+    console.log('lower-case');
+  }
+}
+
+solve('L');
+solve('f');
+solve('h');
