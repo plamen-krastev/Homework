b64b946f4d4cc9f51bd8e2f329472592e759dfc1
plamen-krastev <plamen.krastev@gmail.com>
2024-01-27 03:30:19 +0200

    Basic Syntax, Conditional Statements and Loops - More Exercise

---
+++ Fundamentals - 2024/01 - Basic Syntax, Conditional Statements and Loops/More/03-next-day.js
@@ -0,0 +1,13 @@
+function solve(year, month, day) {
+
+    let nextDay = new Date(year, month - 1, day + 1);
+
+    let theYear = nextDay.getFullYear();
+    let theMonth = nextDay.getMonth();
+    let theDay = nextDay.getDate();
+
+    console.log(`${theYear}-${theMonth + 1}-${theDay}`);
+}
+
+solve(2016, 9, 30);
+solve(2020, 3, 24);
---
+++ Fundamentals - 2024/01 - Basic Syntax, Conditional Statements and Loops/More/demo.js
@@ -0,0 +1,10 @@
+// const date1 = new Date();
+
+const date1 = new Date('December 17, 1995 03:24:00');
+const date3 = new Date('December 17, 1995 03:24:00');
+const date2 = new Date('1995-12-17T03:24:00');
+const date4 = new Date('1995-12-17T03:24:00');
+
+console.log(date1);
+console.log(date2);
+
