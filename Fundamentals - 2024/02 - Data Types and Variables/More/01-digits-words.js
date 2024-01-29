8d9f699d4fa759a1f58b8af014ac8f655a398d9b
plamen-krastev <plamen.krastev@gmail.com>
2024-01-29 18:53:06 +0200

    Data Types and Variables - More Exercise

---
+++ Fundamentals - 2024/02 - Data Types and Variables/More/01-digits-words.js
@@ -0,0 +1,43 @@
+function solve(str) {
+
+    let num = 0;
+
+    switch (str) {
+        case "one":
+            num = 1;
+            break;
+        case "two":
+            num = 2;
+            break;
+        case "three":
+            num = 3;
+            break;
+        case "four":
+            num = 4;
+            break;
+        case "five":
+            num = 5;
+            break;
+        case "six":
+            num = 6;
+            break;
+        case "seven":
+            num = 7;
+            break;
+        case "eight":
+            num = 8;
+            break;
+        case "nine":
+            num = 9;
+            break;
+        case "zero":
+            num = 0;
+            break;
+    }
+
+    console.log(num);
+}
+
+solve('nine');
+solve('two');
+solve('zero');
