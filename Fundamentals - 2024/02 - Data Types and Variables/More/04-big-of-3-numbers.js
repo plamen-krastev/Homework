98bcb83558454875a505a61d668ff0f304973e09
plamen-krastev <plamen.krastev@gmail.com>
2024-01-29 21:50:08 +0200

    Data Types and Variables - More Exercise

---
+++ Fundamentals - 2024/02 - Data Types and Variables/More/04-big-of-3-numbers.js
@@ -0,0 +1,18 @@
+function solve(num1, num2, num3) {
+
+    if (num1 > num2 && num1 > num3) {
+        console.log(num1);
+    } else if (num2 > num1 && num2 > num3) {
+        console.log(num2);
+    } else if (num3 > num1 && num3 > num1) {
+        console.log(num3);
+    } else {
+        console.log(num1);
+    }
+
+}
+
+solve(-2, 7, 3);
+solve(130, 5, 99);
+solve(43, 43.2, 43.1);
+solve(2, 2, 2);
