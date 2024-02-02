72d5493121f60fa92f40077d3d53b8280881b1ea
plamen-krastev <plamen.krastev@gmail.com>
2024-02-02 18:37:37 +0200

    Arrays - Exercise

--- Fundamentals - 2024/02 - Data Types and Variables/More/05-binary-to-decimal.js
+++ Fundamentals - 2024/02 - Data Types and Variables/More/05-binary-to-decimal.js
@@ -1,17 +1,22 @@
 function binaryToDecimal(str) {
     let result = 0;
     let arrNumbers = [128, 64, 32, 16, 8, 4, 2, 1];
-
+    if (str.length !== 8) {
+        for (i = str.length - 1; i < 7; i++) {
+            str += 0;
+        }
+    }
     for (let i = 0; i < str.length; i++) {
         let sign = str[i];
         if (sign === '1') {
             result += arrNumbers[i];
         }
     }
+    
     console.log(result);
 }
 
-binaryToDecimal('00001001');
-binaryToDecimal('00000111');
-binaryToDecimal('11110000');
-binaryToDecimal('11110001');
+binaryToDecimal('11000');
+// binaryToDecimal('11110000');
+// binaryToDecimal('11110000');
+// binaryToDecimal('11110001');
