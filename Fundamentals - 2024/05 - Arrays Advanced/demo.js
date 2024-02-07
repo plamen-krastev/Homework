c63483881d5d108636d808f83b2f1902af98f6f7
plamen-krastev <plamen.krastev@gmail.com>
2024-02-07 19:15:05 +0200

    Arrays Advanced

---
+++ Fundamentals - 2024/05 - Arrays Advanced/demo.js
@@ -0,0 +1,13 @@
+let arr = [4,5,9,10];
+
+let arr1 = arr;
+
+console.log(arr);
+console.log(arr1);
+console.log('+++++++++++++');
+
+arr.shift();
+arr1.pop();
+
+console.log(arr);
+console.log(arr1);
