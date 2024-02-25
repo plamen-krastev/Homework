2a62394262a0da2e1ec5c9c6aa7b610a677f9d24
plamen-krastev <plamen.krastev@gmail.com>
2024-02-25 23:08:33 +0200

    Objects and Classes - Exercise

---
+++ Fundamentals - 2024/06 - Objects and Classes/Exercise/06-make-a-dictionary.js
@@ -0,0 +1,39 @@
+function solve(input) {
+    let startedDictionary = {};
+    for (let info of input) {
+        let wordDefinition = JSON.parse(info);
+        let keys = Object.keys(wordDefinition);
+        let value = Object.values(wordDefinition);
+        startedDictionary[keys] = value[0];
+    }
+
+    let sortedKeys = Object
+        .keys(startedDictionary)
+        .sort((a, b) => a.localeCompare(b));
+
+    let sortedDictionary = {};
+    sortedKeys.forEach(key => sortedDictionary[key] = startedDictionary[key]);
+
+    let dictionary = Object.entries(sortedDictionary);
+    for (let [word, definition] of dictionary) {
+        console.log(`Term: ${word} => Definition: ${definition}`);
+    }
+}
+
+// solve([
+//   '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+//   '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+//   '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+//   '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+//   '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+//   ]
+// );
+
+solve([
+  '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+  '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
+  '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+  '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+  '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} '
+  ]
+);
