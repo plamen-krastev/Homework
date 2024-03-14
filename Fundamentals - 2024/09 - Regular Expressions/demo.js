9b6675870a8d00505ba8330c0ad294b4594ec935
plamen-krastev <plamen.krastev@gmail.com>
2024-03-14 21:10:45 +0200

    Regular Expressions

--- Fundamentals - 2024/09 - Regular Expressions/demo.js
+++ Fundamentals - 2024/09 - Regular Expressions/demo.js
@@ -1,11 +1,12 @@
 function demo() {
-    let text = 'Peter: 123 MMllLark: 456222';
+    let text = 'Peter: 123 Mark: 456';
     let regexp = /([A-Z][a-z]+): (\d+)/g;
-    let matches = text.match(regexp);
-    console.log(matches.length); // 2
-    console.log(matches[0]); // Peter: 123
-    console.log(matches[1]); // Mark: 456
-    console.log(matches);
+    let firstMatch = regexp.exec(text);
+    let secondMatch = regexp.exec(text);
+    console.log(firstMatch[0]) // Peter: 123
+    console.log(firstMatch[1]); // Peter
+    console.log(firstMatch[2]); // 123
+    console.log(firstMatch);
+    console.log(secondMatch);
 }
-
 demo();
