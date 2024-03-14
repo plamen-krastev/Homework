1a09ff55d06555648c105bdf73dd2a5007d42a38
plamen-krastev <plamen.krastev@gmail.com>
2024-03-14 19:24:45 +0200

    Regular Expressions

--- Fundamentals - 2024/09 - Regular Expressions/demo.js
+++ Fundamentals - 2024/09 - Regular Expressions/demo.js
@@ -1,11 +1,11 @@
 function demo() {
-  let text = '_ (Underscores) are   also word characters!';
-  let regex = /\s+/;
-  let array = text.split(regex);
-  console.log(array);
-  let exec = regex.exec(text);
-  console.log(exec);
-
+    let text = 'Peter: 123 MMllLark: 456222';
+    let regexp = /([A-Z][a-z]+): (\d+)/g;
+    let matches = text.match(regexp);
+    console.log(matches.length); // 2
+    console.log(matches[0]); // Peter: 123
+    console.log(matches[1]); // Mark: 456
+    console.log(matches);
 }
 
 demo();
