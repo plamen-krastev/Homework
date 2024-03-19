71ebc61adac966a1cfd2bc306758f4a14b6eba02
plamen-krastev <plamen.krastev@gmail.com>
2024-03-19 22:54:26 +0200

    Regular Expressions - Exercise

--- Fundamentals - 2024/09 - Regular Expressions/Exercise/05-nether-realms.js
+++ Fundamentals - 2024/09 - Regular Expressions/Exercise/05-nether-realms.js
@@ -1,13 +1,38 @@
 function netherRealms(data) {
-  let patternFloatNum = /[-+]?\d+(\.\d+)?/;
-  let demons = data.split(',');
-  for (let demon of demons) {
-    // console.log(demon.trim());
-    demon = demon.trim();
-    if (!demon.includes(' ')) {
-      console.log(demon);
+    let patternNumbers = /[-+]?\d+(\.\d+)?/g;
+    let patternHealth = /[^+-.*/\d]/g;
+    let patternDamage = /[*/]/g;
+    let demons = data.split(',');
+    let demonsArray = [];
+    for (let demon of demons) {
+        demon = demon.trim();
+        if (!demon.includes(' ')) {
+            let health = 0;
+            let damage = 0;
+            let curChar = '';
+            let curNumber;
+            let curOperation;
+            while ((curNumber = patternNumbers.exec(demon)) !== null) {
+                damage += Number(curNumber[0]);
+            }
+            while ((curChar = patternHealth.exec(demon))) {
+                health += curChar[0].charCodeAt();
+            }
+            while ((curOperation = patternDamage.exec(demon)) !== null) {
+                if (curOperation[0] === '*') {
+                    damage *= 2;
+                } else if (curOperation[0] === '/') {
+                    damage /= 2;
+                }
+            }
+            let curDemon = [demon, health, damage];
+            demonsArray.push(curDemon);
+        }
     }
-  }
+    demonsArray.sort((a, b) => a[0][0].localeCompare(b[0][0]));
+    for (let [name, health, damage] of demonsArray) {
+        console.log(`${name} - ${health} health, ${damage.toFixed(2)} damage`);
+    }
 }
 
 netherRealms('M3ph-0.5s-0.5t0.0**');
