73c585855352cf7784511249bf8398d60cee76ac
plamen-krastev <plamen.krastev@gmail.com>
2024-03-23 13:01:20 +0200

    Final Exam Preparation

---
+++ Fundamentals - 2024/Final Exam Preparation/11-world-tour.js
@@ -0,0 +1,37 @@
+function worldTour(data) {
+    let stops = data.shift();
+    let command = data.shift();
+    while (command !== 'Travel') {
+        let token = command.split(':')
+        let action = token.shift();
+        if (action === 'Add Stop') {
+            let idx = Number(token[0]);
+            let town = token[1];
+            if (idx >= 0 && idx < stops.length) {
+                stops = stops.slice(0, idx) + town + stops.slice(idx);
+                console.log(stops);
+            }
+        } else if (action === 'Remove Stop') {
+            let [startIdx, finalIdx] = token.map(Number);
+            if (startIdx >= 0 && finalIdx < stops.length) {
+                stops = stops.slice(0, startIdx) + stops.slice(finalIdx + 1);
+            }
+            console.log(stops);
+        } else if (action === 'Switch') {
+            let [curString, newString] = token;
+            while (stops.includes(curString)) {
+                stops = stops.replace(curString, newString);
+            }
+            console.log(stops);
+        }
+        command = data.shift();
+        if (command === 'Travel') {
+            console.log(`Ready for world tour! Planned stops: ${stops}`);
+        }
+    }
+}
+
+worldTour(["Hawai::Cyprys-Greece", "Add Stop:7:Rome", "Remove Stop:11:16", "Switch:Hawai:Bulgaria", "Travel"]);
+// worldTour(
+    // ['Albania:Bulgaria:Cyprus:Deuchland', 'Add Stop:3:Nigeria', 'Remove Stop:4:8', 'Switch:Albania: Azərbaycan', 'Travel']
+// );
