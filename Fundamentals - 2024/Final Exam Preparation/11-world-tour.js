0b559415092cf3499e3ce22591562e53c4f1daee
plamen-krastev <plamen.krastev@gmail.com>
2024-03-24 08:02:35 +0200

    Final Exam Preparation

--- Fundamentals - 2024/Final Exam Preparation/11-world-tour.js
+++ Fundamentals - 2024/Final Exam Preparation/11-world-tour.js
@@ -29,7 +29,7 @@ function worldTour(data) {
     }
 }
 
-  worldTour(["Hawai::Cyprys-Greece", "Add Stop:7:Rome", "Remove Stop:11:16", "Switch:Hawai:Bulgaria", "Travel"]);
+  worldTour(["Hawai::Cyprys-Greece", "Add Stop:7:Rome", "Remove Stop:11:16", "Add Stop:7:Hawai", "Switch:Hawai:Bulgaria", "Travel"]);
   // worldTour(
   //     ['Albania:Bulgaria:Cyprus:Deuchland', 'Add Stop:3:Nigeria', 'Remove Stop:4:8', 'Switch:Albania: Azərbaycan', 'Travel']
   // );
