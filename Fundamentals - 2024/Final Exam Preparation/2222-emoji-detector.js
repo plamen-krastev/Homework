86851244e1211f6fc08ced8ec7e5e0035157b791
plamen-krastev <plamen.krastev@gmail.com>
2024-03-30 10:41:38 +0200

    Final Exam Preparation

--- Fundamentals - 2024/Final Exam Preparation/2222-emoji-detector.js
+++ Fundamentals - 2024/Final Exam Preparation/2222-emoji-detector.js
@@ -15,28 +15,27 @@ function solve(data) {
     }
 
     let emojiArray = [];
-    let emojiMatch = string.match(emojiPattern);
-    for (let emoji of emojiMatch) {
+    let emojiMatch = emojiPattern.exec(string);
+    while (emojiMatch !== null) {
+        // emoji = { Name: coolness };
+        let emoji = emojiMatch.groups.emoji;
         let sum = 0;
-        for (let i = 2; i < emoji.length - 2; i++) {
-            let num = emoji[i].charCodeAt();
+        for (let symbol of emoji) {
+            let num = symbol.charCodeAt();
             sum += num;
         }
         let emojiObject = {
             [emoji]: sum
         };
         emojiArray.push(emojiObject);
+        emojiMatch = emojiPattern.exec(string);
     }
     console.log(`Cool threshold: ${coolThresholdSum}`);
     console.log(
         `${emojiArray.length} emojis found in the text. The cool ones are:`
     );
     for (let emoji of emojiArray) {
-        let emojiKey = Object.keys(emoji);
-        let emojiValue = Object.values(emoji);
-        if (emojiValue[0] > coolThresholdSum) {
-            console.log(emojiKey.join(''));
-        }
+      console.log(emoji);
     }
 }
 
@@ -44,15 +43,15 @@ solve(["In the Sofia Zoo there are 311 animals in total! ::Smiley:: This include
         "Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 dif" +
         "ferent types of :Snak::Es::. ::Mooning:: **Shy**"]);
 
-solve(
-    ["5, 4, 3, 2, 1, go! The 1-th consecutive banana-eating contest has begun! ::Joy" +
-            ":: **Banana** ::Wink:: **Vali** ::valid_emoji::"]
-);
+// solve(
+//     ["5, 4, 3, 2, 1, go! The 1-th consecutive banana-eating contest has begun! ::Joy" +
+//             ":: **Banana** ::Wink:: **Vali** ::valid_emoji::"]
+// );
 
-solve(
-    ["It is a long established fact that 1 a reader will be distracted by 9 the read" +
-            "able content of a page when looking at its layout. The point of using ::LoremI" +
-            "psum:: is that it has a more-or-less normal 3 distribution of 8 letters, as op" +
-            "posed to using 'Content here, content 99 here', making it look like readable *" +
-            "*English**."]
-);
+// solve(
+//     ["It is a long established fact that 1 a reader will be distracted by 9 the read" +
+//             "able content of a page when looking at its layout. The point of using ::LoremI" +
+//             "psum:: is that it has a more-or-less normal 3 distribution of 8 letters, as op" +
+//             "posed to using 'Content here, content 99 here', making it look like readable *" +
+//             "*English**."]
+// );
