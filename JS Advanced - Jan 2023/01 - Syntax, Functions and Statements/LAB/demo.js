4716a7c120f99a942f71ae0b045229fe836ba64c
plamen-krastev <plamen.krastev@gmail.com>
2024-04-06 12:56:57 +0300

    Syntax, Functions and Statements - Lab

--- JS Advanced - Jan 2023/01 - Syntax, Functions and Statements/LAB/demo.js
+++ JS Advanced - Jan 2023/01 - Syntax, Functions and Statements/LAB/demo.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function demo() {
   var a = 5;
   console.log(a);
@@ -9,4 +11,12 @@ demo();
 
 let b = 3;
 console.log(b);
-console.log(b);
+
+console.log(Boolean(''));
+
+// console.log(a);
+
+console.log(k);
+var k = 7;
+console.log(k);
+
