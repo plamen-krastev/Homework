eebf18852b9dc943552849a4bf111f18dda35f77
plamen-krastev <plamen.krastev@gmail.com>
2024-04-07 16:00:16 +0300

    Syntax, Functions and Statements - Exercise

---
+++ JS Advanced - Jan 2023/01 - Syntax, Functions and Statements/Exercise/07-cooking-by-numbers.js
@@ -0,0 +1,30 @@
+function solve(...data) {
+    let number = Number(data[0]);
+    for (let operation of data) {
+        switch (operation) {
+            case 'chop':
+                number /= 2;
+                console.log(number);
+                break;
+            case 'dice':
+                number = Math.sqrt(number);
+                console.log(number);
+                break;
+            case 'spice':
+                number++;
+                console.log(number);
+                break;
+            case 'bake':
+                number *= 3;
+                console.log(number);
+                break;
+            case 'fillet':
+                number = number * 0.8;
+                console.log(number);
+                break;
+        }
+    }
+}
+
+solve('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
