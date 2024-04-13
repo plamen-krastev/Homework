7f6c3a8beb5a613dc152cd19d403fed4d4152b83
plamen-krastev <plamen.krastev@gmail.com>
2024-04-13 11:22:55 +0300

    Arrays and Nested Arrays - Exercise

--- JS Advanced - Jan 2023/02 - Arrays and Nested Arrays/Exercise/demo.js
+++ JS Advanced - Jan 2023/02 - Arrays and Nested Arrays/Exercise/demo.js
@@ -1,48 +1,11 @@
-function solve(data) {
-    let isTrue = true;
+function isBigEnough(element, index, array) {
+    return element >= 10;
+  }
+//   console.log([12, 5, 8, 130, 44].every(isBigEnough)); // false
+//   console.log([12, 54, 18, 130, 44].every(isBigEnough)); // true
 
-    let rowsSum = [];
-    let colsSum = [];
-    for (let i = 0; i < data.length; i++) {
-        let curColSum = 0;
-        let curRowSum = 0;
-        for (let j = 0; j < data.length; j++) {
-            curColSum += data[j][i];
-            curRowSum += data[i][j];
-        }
-        rowsSum.push(curRowSum);
-        colsSum.push(curColSum);
-    }
-
-    let isEqualRows = rowsSum.every((value, _, arr) => value === arr[0]);
-    let isEqualCols = colsSum.every((value, _, arr) => value === arr[0]);
-
-    for (let i = 0; i < rowsSum.length; i++) {
-        if (rowsSum[i] !== colsSum[i]) {
-            isTrue = false;
-        }
-    }
-    if (isEqualRows && isEqualCols && isTrue) {
-        console.log('true');
-    } else {
-        console.log('false');
-    }
-}
-
-solve([
-  [45, 55, 66],
-  [66, 55, 45],
-  [55, 56, 55],
-]);
-
-solve([
-  [11, 32, 45],
-  [21, 0, 1],
-  [21, 1, 1],
-]);
-
-// solve([
-//   [1, 0, 0],
-//   [0, 0, 1],
-//   [0, 1, 0],
-// ]);
+//   console.log([11, 14, 55, 88, 92, 1].every((el, idx, arr) => el >= 10));
+//   console.log([11, 14, 55, 88, 92, 1].every((el, idx, arr) => el == arr[0]));
+  console.log([11, 11, 11, 11, 11, 11].every((el, idx, arr) => arr[idx] === arr[0]));
+  console.log([11, 11, 11, 11, 11, '11'].every((el, idx, arr) => el === arr[0]));
+  
