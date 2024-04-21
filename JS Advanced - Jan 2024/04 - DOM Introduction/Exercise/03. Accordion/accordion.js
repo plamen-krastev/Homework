65d05c7c48ebd038b682b3f9cd1555424daa70cd
plamen-krastev <plamen.krastev@gmail.com>
2024-04-21 20:07:32 +0300

    DOM Introduction - Exercise

--- JS Advanced - Jan 2024/04 - DOM Introduction/Exercise/03. Accordion/accordion.js
+++ JS Advanced - Jan 2024/04 - DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+  const refToButton = document.getElementsByClassName("button")[0];
+  const refToExtra = document.getElementById("extra");
+
+  if (refToButton.textContent === "More") {
+    refToExtra.style.display = "block";
+    refToButton.textContent = "Less";
+  } else {
+    refToExtra.style.display = "none";
+    refToButton.textContent = "More";
+  }
 }
