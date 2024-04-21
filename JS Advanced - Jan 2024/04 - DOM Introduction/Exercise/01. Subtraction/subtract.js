ad768912c199323390bd2bc6ad982c08c5ec5b22
plamen-krastev <plamen.krastev@gmail.com>
2024-04-21 18:28:42 +0300

    DOM Introduction - Exercise

--- JS Advanced - Jan 2024/04 - DOM Introduction/Exercise/01. Subtraction/subtract.js
+++ JS Advanced - Jan 2024/04 - DOM Introduction/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    console.log('TODO:...');
+  const num1Ref = document.querySelector("#firstNumber");
+  const num2Ref = document.querySelector("#secondNumber");
+  const result = document.querySelector("#result");
+
+  let num1 = Number(num1Ref.value);
+  let num2 = Number(num2Ref.value);
+  let subtract = num1 - num2;
+  result.textContent = subtract;
 }
