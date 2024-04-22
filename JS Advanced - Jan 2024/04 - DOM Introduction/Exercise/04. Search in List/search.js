5d011278088714c29980fa7da4492766eaa38bcd
plamen-krastev <plamen.krastev@gmail.com>
2024-04-22 19:28:18 +0300

    DOM Introduction - Exercise

--- JS Advanced - Jan 2024/04 - DOM Introduction/Exercise/04. Search in List/search.js
+++ JS Advanced - Jan 2024/04 - DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO
+  let searchItem = document.getElementById("searchText").value;
+  const arrayFromElements = Array.from(document.getElementById("towns").children);
+  let matches = 0;
+  for (let town of arrayFromElements) {
+    if (town.textContent.includes(searchItem)) {
+      console.log(typeof town.textContent);
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+      matches++;
+    }
+  }
+  document.getElementById("searchText").value = "";
+  document.getElementById("result").textContent = `${matches} matches found`;
 }
