588b83017c32dab5e633ff3eb53b76c0cf5ce284
plamen-krastev <plamen.krastev@gmail.com>
2024-04-23 23:12:45 +0300

    DOM Manipulation and Events - Lab

--- JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/01. List-Of-Items/app.js
+++ JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/01. List-Of-Items/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+  const text = document.getElementById("newItemText").value;
+  const li = document.createElement("li");
+  //   li.appendChild(document.createTextNode(text));
+  li.textContent = text;
+  //   console.log(li.textContent);
+  const parentElement = document.getElementById("items");
+  parentElement.appendChild(li);
+  document.getElementById("newItemText").value = "";
+  // create new element, adding text and append it to the parent element
+
+  //   if (text) {
+  //     newLiElement.textContent = text;
+  //     parentElement.append(newLiElement);
+  //     addElement.value = "";
+  //   }
+  //   newLiElement.textContent = text;
+  //   parentElement.append(newLiElement);
 }
