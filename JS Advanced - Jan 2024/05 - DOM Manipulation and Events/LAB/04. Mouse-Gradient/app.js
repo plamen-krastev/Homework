a9c77dc957e4f56680a71b18bc1115f1f79efca1
plamen-krastev <plamen.krastev@gmail.com>
2024-04-26 08:40:31 +0300

    DOM Manipulation and Events - Lab

--- JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/04. Mouse-Gradient/app.js
+++ JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  // get ref
+  let gradient = document.getElementById("gradient");
+  gradient.addEventListener("mousemove", gradientMove);
+  gradient.addEventListener("mouseout", gradientOut);
+
+  // both functions gradientMove and gradientOut
+  function gradientMove(event) {
+    const gradientBoxWidth = event.target.clientWidth;
+    const positionOfTheMouse = event.offsetX / (gradientBoxWidth - 1);
+    const percentage = Math.trunc(positionOfTheMouse * 100);
+    document.getElementById("result").textContent = percentage + "%";
+  }
+  function gradientOut(event) {
+    document.getElementById("result").textContent = "";
+  }
 }
