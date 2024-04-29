8d42735cf92fecb5246f7b449fa7ca999c63fc1b
plamen-krastev <plamen.krastev@gmail.com>
2024-04-29 12:59:50 +0300

    DOM Manipulation and Events - Lab

--- JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/05. Highlight-Active/app.js
+++ JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+  let divRef = document.querySelectorAll("div div");
+  for (let div of divRef) {
+    div.addEventListener("mouseover", highlight);
+    div.addEventListener("mouseout", clearHighlight);
+  }
+  function highlight(even) {
+    even.currentTarget.classList.toggle("focused");
+  }
+  function clearHighlight(even) {
+    even.currentTarget.classList.remove("focused");
+  }
 }
