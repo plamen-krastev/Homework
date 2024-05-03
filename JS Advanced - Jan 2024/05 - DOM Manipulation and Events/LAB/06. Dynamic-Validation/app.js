619323feee5154b306f42e619e3e5bee3f38102e
plamen-krastev <plamen.krastev@gmail.com>
2024-05-03 08:49:35 +0300

    DOM Manipulation and Events - Exercise

--- JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/06. Dynamic-Validation/app.js
+++ JS Advanced - Jan 2024/05 - DOM Manipulation and Events/LAB/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+  const refEmail = document.getElementById("email");
+  const getText = refEmail.value;
+  let regexPattern =
+    /([A-Za-z0-9_]{2,})@([A-Za-z0-9_]{2,}).([A-Za-z0-9_]{2,})/g;
+  let match = getText.match(regexPattern);
+
+//   refEmail.addEventListener("focus", checkEmail);
+  refEmail.addEventListener("blur", checkEmail);
+  function checkEmail(event) {
+    if (match === null) {
+      event.currentTarget.classList.toggle("error");
+    } 
+  }
+  console.log(match);
 }
+
+// validate("asdasdasfdsaf@sdfsaf.vom");
