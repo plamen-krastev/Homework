ff206048f7fe0d364e9083e2ae08466b568b6b52
plamen-krastev <plamen.krastev@gmail.com>
2024-05-03 15:31:10 +0300

    DOM Manipulation and Events - Exercise

--- JS Advanced - Jan 2024/05 - DOM Manipulation and Events/Exercise/01. Sections/script.js
+++ JS Advanced - Jan 2024/05 - DOM Manipulation and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,30 @@
 function create(words) {
-   console.log('TODO:...');
+  // getting reference
+  const contentRef = document.getElementById("content");
+
+  for (let word of words) {
+    // creating both elements
+    let divEl = document.createElement("div");
+    let pEl = document.createElement("p");
+
+    // adding text in pEl and set display to "none"
+    pEl.textContent = word;
+    pEl.style.display = "none";
+
+    // append elements
+    divEl.appendChild(pEl);
+    contentRef.appendChild(divEl);
+
+    // adding listener
+    divEl.addEventListener("click", clickHandler);
+  }
+  function clickHandler(event) {
+    // getting target (div element)
+    let target = event.currentTarget;
+    // getting child elements
+    let children = target.children;
+    // getting child (p element)
+    let p = children[0];
+    p.style.display = "block";
+  }
 }
