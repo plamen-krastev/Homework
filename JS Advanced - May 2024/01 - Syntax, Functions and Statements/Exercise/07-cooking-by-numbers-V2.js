0695c3bda710c670b802f9f618b6854b7ea2663b
plamen-krastev <plamen.krastev@gmail.com>
2024-05-26 08:48:47 +0300

    Syntax, Functions and Statements - Exercise

---
+++ JS Advanced - May 2024/01 - Syntax, Functions and Statements/Exercise/07-cooking-by-numbers-V2.js
@@ -0,0 +1,48 @@
+function cookingByNumbers(
+  numAsString,
+  operator1,
+  operator2,
+  operator3,
+  operator4,
+  operator5
+) {
+  let num = Number(numAsString);
+
+  let actions = {
+    operator1,
+    operator2,
+    operator3,
+    operator4,
+    operator5,
+
+    chop: function (num) {
+      num /= 2;
+      return num;
+    },
+    dice(num) {
+      num = Math.sqrt(num);
+      return num;
+    },
+    spice(num) {
+      num += 1;
+      return num;
+    },
+    bake: function (num) {
+      num *= 3;
+      return num;
+    },
+    fillet(num) {
+      num *= 0.8;
+      return num;
+    },
+  };
+
+  for (let i = 1; i <= 5; i++) {
+    let curOperation = actions["operator" + i];
+    num = actions[curOperation](num);
+    console.log(num);
+  }
+}
+
+cookingByNumbers("32", "chop", "chop", "chop", "chop", "chop");
+cookingByNumbers("9", "dice", "spice", "chop", "bake", "fillet");
--- JS Advanced - May 2024/03 - Objects and Composition/demo.js
+++ JS Advanced - May 2024/03 - Objects and Composition/demo.js
@@ -1,9 +1,44 @@
 function createRect(width, height) {
   const rect = { width, height };
   rect.getArea = () => {
-  return rect.width * rect.height;
+    return rect.width * rect.height;
   };
   return rect;
-  }
+}
 
-  console.log(createRect(12, 15).getArea());
+console.log(createRect(12, 15).getArea());
+
+console.log("\n##########################\n");
+
+// Object destructuring
+const obj = { a: 1, b: 2, c: 3, d: 4 };
+const { c, ...props } = obj;
+const modifiedObj = {
+  ...props,
+  c: 12,
+};
+console.log(obj); // { a: 1, b: 2, c: 3, d: 4 }
+console.log(modifiedObj); // { a: 1, b: 2, d: 12, c: 12 }
+modifiedObj.f = 18;
+console.log(modifiedObj);
+
+console.log("\n##########################\n");
+
+// Object References
+let x = { name: "John" };
+let y = x;
+y.name = "Steve";
+console.log(x.name);
+
+console.log("\n##########################\n");
+
+// sorting helper
+const compareNumbers = {
+  ascending: (a, b) => a - b,
+  descending: (a, b) => b - a,
+};
+let arr = [24, 35, 88, 33, 1434, 346];
+console.log(arr.sort(compareNumbers.descending));
+
+console.log("\n##########################\n");
+
