e184587dde31220f6fbbe625dcf2c9220f1a3677
plamen-krastev <plamen.krastev@gmail.com>
2024-05-31 17:09:04 +0300

    Objects and Composition - Lab

--- JS Advanced - May 2024/03 - Objects and Composition/LAB/05-assembly-line.js
+++ JS Advanced - May 2024/03 - Objects and Composition/LAB/05-assembly-line.js
@@ -1,11 +1,45 @@
 function createAssemblyLine() {
-  const result = {
-    temp: 21,
-    hasClima() {
-      
+  const decoration = {
+    hasClima(car) {
+      car.temp = 21;
+      car.tempSettings = 21;
+      car.adjustTemp = function () {
+        // while (car.temp !== car.tempSettings) {
+        //   if (car.temp < car.tempSettings) {
+        //     car.temp++;
+        //   } else {
+        //     car.temp--;
+        //   }
+        // }
+        if (car.temp < car.tempSettings) {
+          car.temp++;
+        } else if (car.temp > car.tempSettings) {
+          car.temp--;
+        }
+      };
+    },
+    hasAudio(car) {
+      car.currentTrack = {};
+      car.nowPlaying = function () {
+        console.log(
+          `Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`
+        );
+      };
+    },
+    hasParktronic(car) {
+      car.checkDistance = function (distance) {
+        if (distance < 0.1) {
+          console.log("Beep! Beep! Beep!");
+        } else if (0.1 <= distance && distance < 0.25) {
+          console.log("Beep! Beep!");
+        } else if (0.25 <= distance && distance < 0.5) {
+          console.log("Beep!");
+        }
+      };
     },
   };
-  return result;
+
+  return decoration;
 }
 
 const assemblyLine = createAssemblyLine();
@@ -16,7 +50,20 @@ const myCar = {
 };
 
 assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+  name: "Never Gonna Give You Up",
+  artist: "Rick Astley",
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
 console.log(myCar);
-// myCar.tempSettings = 18;
-// myCar.adjustTemp();
-// console.log(myCar.temp);
