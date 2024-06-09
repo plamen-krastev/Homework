9028d1dea6afc72bd0e5ff8254fd6062a468880d
plamen-krastev <plamen.krastev@gmail.com>
2024-06-09 11:24:31 +0300

    DOM Introduction - Lab

--- JS Advanced - May 2024/04 - DOM Introduction/LAB/06.Sum Table/SumTable.js
+++ JS Advanced - May 2024/04 - DOM Introduction/LAB/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+  const refTD = document.querySelectorAll("tbody tr td");
+  const refSum = document.getElementById("sum");
+  let sum = 0;
+  for (let i = 1; i < refTD.length - 2; i += 2) {
+    let curNum = Number(refTD[i].textContent);
+    sum += curNum;
+  }
+  refSum.textContent = sum;
 }
--- JS Advanced - May 2024/04 - DOM Introduction/LAB/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced - May 2024/04 - DOM Introduction/LAB/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,7 @@
 function extract(content) {
-
+  const ref = document.getElementById("content");
+  const textContent = ref.textContent;
+  const pattern = /\([A-Za-z 0-9-]+\)/g;
+  let result = textContent.match(pattern);
+  return result.join("; ");
 }
