027d61a6287500566b879273b860809d09ace460
plamen-krastev <plamen.krastev@gmail.com>
2024-06-09 16:40:58 +0300

    DOM Introduction - Exercise

--- JS Advanced - May 2024/04 - DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ JS Advanced - May 2024/04 - DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  const getInputText = document.getElementById("text").value;
+  const getConvention = document.getElementById("naming-convention").value;
+  const refResult = document.getElementById("result");
+
+  let splitText = getInputText.split(" ").map((el) => el.toLowerCase());
+  let result = [];
+
+  if (getConvention === "Camel Case") {
+    for (let i = 0; i < splitText.length; i++) {
+      let curWord = splitText[i];
+      if (i === 0) {
+        result.push(curWord);
+      } else {
+        let word = curWord[0].toUpperCase() + curWord.slice(1);
+        result.push(word);
+      }
+    }
+    refResult.textContent = result.join("");
+  } else if (getConvention === "Pascal Case") {
+    for (let word of splitText) {
+      word = word[0].toUpperCase() + word.slice(1);
+      result.push(word);
+    }
+    refResult.textContent = result.join("");
+  } else {
+    refResult.textContent = "Error!";
+  }
 }
