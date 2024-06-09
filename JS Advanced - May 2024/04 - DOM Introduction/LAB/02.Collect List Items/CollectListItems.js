bc5cf09b46fa89921378442eef252eaafd9154fa
plamen-krastev <plamen.krastev@gmail.com>
2024-06-09 08:48:01 +0300

    DOM Introduction - Lab

--- JS Advanced - May 2024/04 - DOM Introduction/LAB/02.Collect List Items/CollectListItems.js
+++ JS Advanced - May 2024/04 - DOM Introduction/LAB/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,11 @@
 function extractText() {
-    // TODO
+  let refLi = document.getElementById("items").children;
+  let refTextArea = document.getElementById("result");
+
+  let textContent = "";
+  for (let el of refLi) {
+    textContent += el.textContent + "\n";
+  }
+
+  refTextArea.textContent = textContent;
 }
