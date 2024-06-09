302a2dcf8b89828b0505810b725e29ef9bf7b95c
plamen-krastev <plamen.krastev@gmail.com>
2024-06-09 09:04:57 +0300

    DOM Introduction - Lab

--- JS Advanced - May 2024/04 - DOM Introduction/LAB/03.Sum Numbers/SumNumbers.js
+++ JS Advanced - May 2024/04 - DOM Introduction/LAB/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,12 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  let refNum1 = document.getElementById("num1");
+  let refNum2 = document.getElementById("num2");
+  let refSum = document.getElementById("sum");
+
+  let num1 = Number(refNum1.value);
+  let num2 = Number(refNum2.value);
+
+  let sum = num1 + num2;
+
+  refSum.value = sum;
 }
