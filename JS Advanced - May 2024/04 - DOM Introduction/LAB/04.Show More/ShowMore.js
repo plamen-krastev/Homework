89366e5ab922349d5ddfb7ab0d2e9b5fae17d815
plamen-krastev <plamen.krastev@gmail.com>
2024-06-09 10:33:01 +0300

    DOM Introduction - Lab

--- JS Advanced - May 2024/04 - DOM Introduction/LAB/04.Show More/ShowMore.js
+++ JS Advanced - May 2024/04 - DOM Introduction/LAB/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+  let refReadMore = document.getElementById("more");
+  let refHiddenText = document.getElementById("text");
+  refReadMore.style.display = "none";
+  refHiddenText.style.display = "inline";
 }
--- JS Advanced - May 2024/04 - DOM Introduction/LAB/05.Colorize Table/ColorizeTable.js
+++ JS Advanced - May 2024/04 - DOM Introduction/LAB/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+  const refTDElement = document.querySelectorAll("table tr");
+  for (let i = 0; i < refTDElement.length; i++) {
+    if (i % 2 !== 0) {
+      refTDElement[i].style.background = "teal";
+    }
+  }
 }
