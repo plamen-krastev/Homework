192dcfedff71e6cf704ce89771e850ebd090fa42
plamen-krastev <plamen.krastev@gmail.com>
2024-06-11 06:00:58 +0300

    DOM Introduction - Exercise

--- JS Advanced - May 2024/04 - DOM Introduction/Exercise/04. Search in List/search.js
+++ JS Advanced - May 2024/04 - DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+  const refUl = document.getElementById("towns");
+  const refSearch = document.getElementById("searchText");
+  const refResult = document.getElementById("result");
+
+  let searchedText = refSearch.value;
+  const liElementCollection = refUl.children;
+  let matchCount = 0;
+
+  if (searchedText !== "") {
+    for (let el of liElementCollection) {
+      if (el.textContent.includes(searchedText)) {
+        el.style.fontWeight = "bold";
+        el.style.textDecoration = "underline";
+        matchCount++;
+      }
+    }
+  }
+
+  if (matchCount > 0) {
+    refResult.textContent = `${matchCount} matches found`;
+  }
+  refSearch.value = "";
 }
