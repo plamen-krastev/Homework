1ed47165bd6dea2984f70e45d581f5b4298c6f19
plamen-krastev <plamen.krastev@gmail.com>
2024-06-14 09:42:26 +0300

    DOM Introduction - Exercise

--- JS Advanced - May 2024/04 - DOM Introduction/Exercise/05. Table - Search Engine/solution.js
+++ JS Advanced - May 2024/04 - DOM Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const refTbody = document.querySelectorAll("table tbody tr");
+    const refSearchField = document.querySelector("#searchField");
+    const searchedText = refSearchField.value;
 
-   }
+    for (let row of refTbody) {
+      row.classList.remove("select");
+    }
+
+    for (let row of refTbody) {
+      for (let cell of row.children) {
+        let cellTextContent = cell.textContent;
+        if (cellTextContent.includes(searchedText)) {
+          cell.parentElement.classList.add("select");
+        }
+      }
+    }
+    refSearchField.value = "";
+  }
 }
