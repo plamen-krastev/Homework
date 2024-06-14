1c9f40afec70404e80557a974f8185fef36a50bf
plamen-krastev <plamen.krastev@gmail.com>
2024-06-14 13:48:41 +0300

    DOM Introduction - Exercise

--- JS Advanced - May 2024/04 - DOM Introduction/Exercise/06. Format the Text/solution.js
+++ JS Advanced - May 2024/04 - DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  const refResultDiv = document.querySelector("#output");
+  const refInputTextArea = document.querySelector("#input");
+
+  const inputText = refInputTextArea.value;
+  if (inputText.length) {
+    const sentences = inputText.split(".").filter((el) => el.length > 0);
+
+    for (let i = 0; i < sentences.length; i += 3) {
+      let pElement = document.createElement("p");
+      for (let j = 0; j < 3; j++) {
+        let index = i + j;
+        if (index < sentences.length) {
+          let sentence = sentences[index];
+          pElement.textContent += sentence + ".";
+        }
+      }
+      refResultDiv.appendChild(pElement);
+    }
+  }
 }
