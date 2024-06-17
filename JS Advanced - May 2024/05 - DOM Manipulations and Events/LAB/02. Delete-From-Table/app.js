19edae12d2f45b8c4fdc9ca51dfcc47b6a3b8297
plamen-krastev <plamen.krastev@gmail.com>
2024-06-17 14:00:32 +0300

    DOM Manipulation and Events - Lab

--- JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/02. Delete-From-Table/app.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/02. Delete-From-Table/app.js
@@ -1,17 +1,20 @@
 function deleteByEmail() {
   const inputRef = document.querySelector("input[name='email']");
-  const cellsRef = document.querySelectorAll("table tbody tr td");
+  const secondColumn = document.querySelectorAll(
+    "#customers tr td:nth-child(2)"
+  );
+  //   const cellsRef = document.querySelectorAll("table tbody tr td");
   const resultRef = document.querySelector("#result");
 
   const searchedEmail = inputRef.value;
 
-  for (let cell of cellsRef) {
+  for (let cell of secondColumn) {
     if (searchedEmail !== "") {
       if (cell.textContent.includes(searchedEmail)) {
         let parentElement = cell.parentElement;
         parentElement.remove();
         resultRef.textContent = "Deleted.";
-        break;
+        return;
       } else {
         resultRef.textContent = "Not found.";
       }
--- JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/03. Add-Delete/app.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+  // getting text, creating element and getting reference to the parent "ul" element
+  const text = document.querySelector("#newItemText").value;
+  const liElement = document.createElement("li");
+  const ulElement = document.querySelector("#items");
+
+  if (text.length === 0) return;
+
+  // add "li" element and reset value
+  liElement.textContent = text;
+  ulElement.appendChild(liElement);
+  document.querySelector("#newItemText").value = "";
+
+  // create "a" element, adding text for it, adding link for it, append it to the "li" element end set listener for it
+  const aElement = document.createElement("a");
+  const aElementText = document.createTextNode("[Delete]");
+  aElement.href = "#";
+  aElement.appendChild(aElementText);
+  liElement.appendChild(aElement);
+  aElement.addEventListener("click", deleteLiItem);
+
+  // create delete function
+  function deleteLiItem() {
+    liElement.remove();
+  }
 }
