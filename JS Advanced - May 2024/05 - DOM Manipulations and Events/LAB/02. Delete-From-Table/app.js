10609b4484d499ca354eb2f3555ccd483c63c14b
plamen-krastev <plamen.krastev@gmail.com>
2024-06-17 13:03:10 +0300

    DOM Manipulation and Events - Lab

--- JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/02. Delete-From-Table/app.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/02. Delete-From-Table/app.js
@@ -1,3 +1,20 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const inputRef = document.querySelector("input[name='email']");
+  const cellsRef = document.querySelectorAll("table tbody tr td");
+  const resultRef = document.querySelector("#result");
+
+  const searchedEmail = inputRef.value;
+
+  for (let cell of cellsRef) {
+    if (searchedEmail !== "") {
+      if (cell.textContent.includes(searchedEmail)) {
+        let parentElement = cell.parentElement;
+        parentElement.remove();
+        resultRef.textContent = "Deleted.";
+        break;
+      } else {
+        resultRef.textContent = "Not found.";
+      }
+    }
+  }
 }
