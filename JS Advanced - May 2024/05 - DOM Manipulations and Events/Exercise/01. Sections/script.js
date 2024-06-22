5a3bdf95b07ade82d2c9a1d9180e3659eaef43e2
plamen-krastev <plamen.krastev@gmail.com>
2024-06-22 18:59:45 +0300

    DOM Manipulation and Events - Exercise

--- JS Advanced - May 2024/05 - DOM Manipulations and Events/Exercise/01. Sections/script.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+  const divContentRef = document.querySelector("#content");
+
+  for (let word of words) {
+    let divElement = document.createElement("div");
+    let pElement = document.createElement("p");
+
+    divElement.addEventListener("click", displayPElement);
+
+    pElement.textContent = word;
+    pElement.style.display = "none";
+    divElement.appendChild(pElement);
+    divContentRef.appendChild(divElement);
+  }
+
+  function displayPElement(e) {
+    let curPElement = e.currentTarget.firstChild;
+    curPElement.style.display = "block";
+    //  curPElement.style.display === "block"
+    //    ? (curPElement.style.display = "none")
+    //    : (curPElement.style.display = "block");
+  }
 }
