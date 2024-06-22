32a53bdc8311c0a8dafbe72b96025cede7525379
plamen-krastev <plamen.krastev@gmail.com>
2024-06-22 10:49:50 +0300

    DOM Manipulation and Events - Lab

--- JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/07. Shopping-Cart/solution.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/LAB/07. Shopping-Cart/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-   
+  const addButtonRef = document.querySelectorAll(".add-product");
+  const textAreaRef = document.querySelector("textarea");
+  const checkoutRef = document.querySelector(".checkout");
+
+  for (let currentButton of addButtonRef) {
+    currentButton.addEventListener("click", addProduct);
+  }
+
+  let totalPrice = 0;
+  let products = [];
+
+  checkoutRef.addEventListener("click", checkoutOrder);
+
+  function addProduct(e) {
+    const curElement = e.currentTarget.parentElement.parentElement;
+    const product = curElement.querySelector(".product-title").textContent;
+    let price = curElement.querySelector(".product-line-price").textContent;
+    price = Number(price);
+
+    totalPrice += price;
+    products.push(product);
+
+    textAreaRef.textContent += `Added ${product} for ${price.toFixed(2)} to the cart.\n`;
+  }
+
+  function checkoutOrder(e) {
+    let uniqueSet = new Set(products);
+    const productList = Array.from(uniqueSet);
+
+    textAreaRef.textContent += `You bought ${productList.join(", ")} for ${totalPrice.toFixed(2)}.`;
+
+    for (let currentButton of addButtonRef) {
+      currentButton.disabled = true;
+    }
+    e.currentTarget.disabled = true;
+  }
 }
