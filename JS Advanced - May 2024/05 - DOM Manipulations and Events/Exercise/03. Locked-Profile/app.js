b75e97510771e07ab399685a80f9789271af709b
plamen-krastev <plamen.krastev@gmail.com>
2024-06-23 11:04:35 +0300

    DOM Manipulation and Events - Exercise

--- JS Advanced - May 2024/05 - DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,15 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll("button")); // array of buttons
+  buttons.forEach((x) => x.addEventListener("click", clickHandler)); // added listener on each button
+
+  function clickHandler(event) {
+    let target = event.currentTarget; // getting clicked button
+    let hiddenInfo = target.parentElement.querySelector("div"); // getting hidden info
+    let inputRadioChecked = target.parentElement.querySelector("input[type='radio']:checked"); // getting radio button state
+
+    if (inputRadioChecked.value === "unlock") {
+      target.textContent = target.textContent === "Show more" ? "Hide it" : "Show more";
+      hiddenInfo.style.display = hiddenInfo.style.display === "block" ? "none" : "block";
+    }
+  }
 }
