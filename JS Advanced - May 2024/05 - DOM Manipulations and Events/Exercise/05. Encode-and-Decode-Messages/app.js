9738cfa005862edc634f3e445fa0a66baf7d1aa6
plamen-krastev <plamen.krastev@gmail.com>
2024-06-23 13:35:53 +0300

    DOM Manipulation and Events - Exercise

--- JS Advanced - May 2024/05 - DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced - May 2024/05 - DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  // getting references
+  const bothDivRef = document.querySelectorAll("#main div");
+  const encodeMessageTextRef = bothDivRef[0].children[1];
+  const encodeMessageButtonRef = bothDivRef[0].children[2];
+  const decodeMessageTextRef = bothDivRef[1].children[1];
+  const decodeMessageButtonRef = bothDivRef[1].children[2];
+
+  // add event listeners
+  encodeMessageButtonRef.addEventListener("click", encodeMessage);
+  decodeMessageButtonRef.addEventListener("click", decodeMessage);
+
+  function encodeMessage() {
+    let encodeMessageText = "";
+    const message = encodeMessageTextRef.value;
+    for (let letter of message) {
+      let encodeLetterCharCode = letter.charCodeAt() + 1;
+      let encodeLetter = String.fromCharCode(encodeLetterCharCode);
+      encodeMessageText += encodeLetter;
+    }
+
+    encodeMessageTextRef.value = "";
+    decodeMessageTextRef.value = encodeMessageText;
+    // encodeMessageButtonRef.disabled = true;
+    decodeMessageButtonRef.disabled = false;
+  }
+
+  function decodeMessage() {
+    let decodeMessageText = "";
+    const message = decodeMessageTextRef.value;
+    for (let letter of message) {
+      let decodeLetterCharCode = letter.charCodeAt() - 1;
+      let decodeLetter = String.fromCharCode(decodeLetterCharCode);
+      decodeMessageText += decodeLetter;
+    }
+    decodeMessageTextRef.value = decodeMessageText;
+    decodeMessageButtonRef.disabled = true;
+  }
 }
